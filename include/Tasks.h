0b14148d8368b4fd520ccda2835d5b5b95011d5d
ilya-fedorets <85130411+ilya-fedorets@users.noreply.github.com>
2024-09-29 10:33:50 +0300

    Create Tasks.h

---
+++ include/Tasks.h
@@ -0,0 +1,20 @@
+// Copyright 2024 Fedorets Ilya
+
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+
+#include <utility>
+
+double ropeAroundPlanetTask(
+    double planetRadius,
+    double radiusIncrement
+);
+
+double pathAroundPoolAndFencePriceTask(
+    double poolRadius,
+    double pathWidth,
+    double concretePrice,
+    double fencePrice
+);
+
+#endif //INCLUDE_TASKS_H_
