a2fc759a9d8d82ec173e8930d81d254d8fddde02
ilya-fedorets <85130411+ilya-fedorets@users.noreply.github.com>
2024-09-29 10:45:01 +0300

    Update Tasks.cpp

--- src/Tasks.cpp
+++ src/Tasks.cpp
@@ -1,1 +1,20 @@
+// Copyright 2024 Fedorets Ilya
+#include "tasks.h"
+#include "circle.h"
 
+double ropeAroundPlanetTask(double planetRadius, double radiusIncrement) {
+    Circle circle(planetRadius);
+    circle.setFerence(circle.getFerence() + radiusIncrement);
+    return circle.getRadius() - planetRadius;
+}
+
+double pathAroundPoolAndFencePriceTask(
+        double poolRadius,
+        double pathWidth,
+        double concretePrice,
+        double fencePrice) {
+    Circle pool(poolRadius);
+    Circle fence(poolRadius + pathWidth);
+    return (fence.getArea() - pool.getArea()) * concretePrice
+        + fence.getFerence() * fencePrice;
+}
