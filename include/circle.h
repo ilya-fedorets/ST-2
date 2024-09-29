c527048b5a9987b7e59b423b050b16f917d6ceb6
ilya-fedorets <85130411+ilya-fedorets@users.noreply.github.com>
2024-09-29 10:31:43 +0300

    Update circle.h

--- include/circle.h
+++ include/circle.h
@@ -1,7 +1,28 @@
-// Copyright 2022 UNN-CS
+// Copyright 2024 Fedorets Ilya
 #ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
 #include <cstdint>
 
+class Circle {
+ private:
+    double radius;
+    double ference;
+    double area;
+
+ public:
+    explicit Circle(double radius);
+
+    void setRadius(double radius);
+
+    void setFerence(double ference);
+
+    void setArea(double area);
+
+    double getRadius() const;
+
+    double getFerence() const;
+
+    double getArea() const;
+};
 
 #endif  // INCLUDE_CIRCLE_H_
